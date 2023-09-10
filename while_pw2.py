ee2fd5d9495c640e302159601471825ba4fd4355
yukihiro <currydrum@gmail.com>
2023-09-10 21:33:49 +0800

    Change to while retry > 0

--- while_pw2.py
+++ while_pw2.py
@@ -1,21 +1,6 @@
 
 passpw = 'a123456'
 retry = 3
-while True:
-	pw = input('請輸入密碼:')
-	if pw == passpw:
-		print('登入成功！')
-		break
-	else:
-		retry = retry - 1
-		print('密碼錯誤！還有',retry ,'次機會！')
-		if retry == 0:
-			break;
-
-
-'''
-passpw = 'a123456'
-retry = 3
 while retry > 0:
 	pw = input('請輸入密碼:')
 	if pw == passpw:
@@ -24,4 +9,3 @@ while retry > 0:
 	else:
 		retry = retry - 1
 		print('密碼錯誤！還有',retry ,'次機會！')
-'''
